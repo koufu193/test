64a261adf3a2549df33ec33639eda1d06d2531d7
73023297+koufu193@users.noreply.github.com <toufu335@gmail.com>
2021-09-07 15:27:00 +0900

    aaa

--- sources/koufu193/Main.java
+++ sources/koufu193/Main.java
@@ -1,5 +1,5 @@
-public class Main{
-    public static void main(String[] args){
-        System.println("aaa");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aaa");// example
     }
 }
