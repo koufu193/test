dd3bca2b44415ac33e1442b8127a2aba5913c7c3
73023297+koufu193@users.noreply.github.com <toufu335@gmail.com>
2021-09-07 15:27:49 +0900

    test

--- sources/koufu193/Main.java
+++ sources/koufu193/Main.java
@@ -1,5 +1,11 @@
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
     public static void main(String[] args) {
-        System.out.println("aaa");// example
+        Main main = new Main();
+        main.setSize(800, 600);
+        main.setTitle("test");
+        main.setVisible(true);
+        main.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
